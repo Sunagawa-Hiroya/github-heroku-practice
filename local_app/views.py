1912c3d981ecb4f2d6aec75b2a05032df2381a96
Sunagawa-Hiroya <e155746basuke@gmail.com>
2021-02-17 21:06:39 +0900

    modify code

--- local_app/views.py
+++ local_app/views.py
@@ -7,4 +7,10 @@ def testfunc(request):
 
 def create_message(request):
     # print(request.body)
+    print('OK!!!!')
+    if request.method == 'POST':
+        print('this is post method')
+    else:
+        print('this is get method')
+
     return HttpResponse('Lineに返信！！見えてる？, コンソールに出ている？')
