9dbbe70ec987fac2c5ff77fd5617b3940fe60b7b
Sunagawa-Hiroya <e155746basuke@gmail.com>
2021-02-18 20:12:52 +0900

    add .gitignore

--- local_app/views.py
+++ local_app/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponse
+import json
 
 
 def testfunc(request):
     return HttpResponse('Hello my App!!! 自動的にビルドしたよ！')
 
 
+@csrf_exempt
 def create_message(request):
     # print(request.body)
     print('OK!!!!')
     if request.method == 'POST':
         print('this is post method')
+        print('jsondataはこれです: ' + json.loads(request.body.decode('utf-8')))
     else:
         print('this is get method')
 
